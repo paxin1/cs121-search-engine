583a9610ad46fdae0b4e4c9c82c130464dd82455
7even-7 <johnsun691@gmail.com>
2022-05-06 02:24:27 -0700

    break into functions

--- generate_index.py
+++ generate_index.py
@@ -7,14 +7,15 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 from collections import defaultdict
 
-directory = "DEV\\aiclub_ics_uci_edu" #directory path to recurse through
+directory = "D:\\APP_Downloads\\DEV" #directory path to recurse through
 
 frequencies = defaultdict(list) #inverted index (word -> document posting)
-file_count = 0
 
-if __name__ == "__main__":
-    #recursively iterate through all files in given directory
+#recursively iterate through all files in given directory
+def process_directory():
+    file_count = 0
     for root, dirs, files in os.walk(directory):
+
         for file in files:
             file_count += 1
 
@@ -28,10 +29,10 @@ if __name__ == "__main__":
             soup = BeautifulSoup(data["content"], features="html.parser")
             dup_tokens = re.findall('[a-zA-Z0-9]{1,}', soup.get_text())
             dup_tokens = [token.lower() for token in dup_tokens]
-
             #create frequency dict for tokens in current file
             cur_frequencies = {}
             for word in dup_tokens:
+                
                 try:
                     cur_frequencies[word] += 1
                 except:
@@ -40,8 +41,8 @@ if __name__ == "__main__":
             #add words and document postings to inverted index
             for item in cur_frequencies.items():
                 frequencies[item[0]].append({"name": file, "frequency": item[1]})
-
-
+    return file_count
+def create_report(file_count):
     #write basic report numbers to file
     unique_tokens = len(frequencies)
     f = open('report.txt', 'w+')
@@ -49,11 +50,19 @@ if __name__ == "__main__":
     f.write(report_str)
     f.close()
 
+def create_csv_report():
     #write frequency dict to csv file for testing
     f = open('frequencies.csv', 'w+', newline='')
     csv_writer = csv.writer(f)
     csv_writer.writerows(frequencies.items())
-    f.close()
+    f.close()  
+if __name__ == "__main__":
+    count = process_directory()
+    create_report(count)
+    create_csv_report()
+
+
+    
     #for writing to plain text file
     """
     f = open('frequencies.txt', 'w+')
